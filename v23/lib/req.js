28c30aa44877c30dd5db83da22e513b34c311f9f
Yuriy Kostiv <y3kostiv@gmail.com>
2020-01-22 21:02:07 +0200

    Fixed the title

--- v23/lib/req.js
+++ v23/lib/req.js
@@ -1,20 +1,36 @@
 var request = require("request");
 var geourl = "https://geolocation-db.com/json";
-var lat;
-var long;
+var lat, long;
+
 
 request({
     url: geourl,
     json: true
-}, function (error, response, json) {
-
+}, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-        console.log(body);
-        lat = json.latitude;
-        long = json.longitude;
-        console.log(lat,long);
+        lat = body.latitude;
+        long = body.longitude;
+    } else {
+      console.log(error);
     }
-})
-.then(function(result) {
-  console.log(lat,long);
+    console.log(lat,long);
 });
+
+/*
+return new Promise(resolve => {
+    request({
+      url: geourl,
+      json: true
+    }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+          resolve(body);
+      } else {
+          console.log(error);
+        }
+    })
+}).then(value => {
+  lat = value.latitude;
+  long = value.longitude;
+  console.log(lat,long);
+})
+*/
